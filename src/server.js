453cacb2205a53412c779c6f769838301f51fb4f
Ayrton Mugnaini <yrto@outlook.com>
2021-07-28 20:39:55 -0300

    Port config

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ server.use("/", routes);
 
 // listen
 
-server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
